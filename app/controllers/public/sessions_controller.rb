ea6853b3f549672c0789278d0f5342901a1bd489
EC2 Default User <ec2-user@ip-172-31-42-180.ap-northeast-1.compute.internal>
2023-06-03 19:24:04 +0000

    fourth

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,6 +3,10 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def create
+
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -24,4 +28,23 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+
+before_action :customer_state, only: [:create]
+protected
+# 退会しているかを判断するメソッド
+def customer_state
+  # 【処理内容1】 入力されたemailからアカウントを1件取得
+  @customer = Customer.find_by(email: params[:customer][:email])
+  # アカウントを取得できなかった場合、このメソッドを終了する
+  return if !@customer
+  # 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+  # 【処理内容3】
+  if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
+  # createアクションの実行
+  else
+    redirect_to new_customer_registration_path
+
+  end
+end
 end
