a6dcc350add366aca83e2fc9090ef2cf22f008b9
EC2 Default User <ec2-user@ip-172-31-42-180.ap-northeast-1.compute.internal>
2023-06-06 15:22:17 +0000

    fourth

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,9 +4,15 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    
+    @items = Item.new(item_params)
+    @item.user_id = current_user.id
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
   def index
   end
 
@@ -15,4 +21,10 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction)
+  end
+
 end
